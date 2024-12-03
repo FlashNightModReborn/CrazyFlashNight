c9dd97b3d7aa24c974228cc3f28ca0f1bbfaf10d
Flash-Night <138298913+Flash-Night@users.noreply.github.com>
2024-12-03 20:09:08 +0800

    将已模板化的原版关卡文件移入unused

--- flashswf/levels/readnames.py
+++
@@ -1,8 +0,0 @@
-with open('C:/Users/lsy20/Downloads/levels.txt','r') as f1:
-    with open('C:/Users/lsy20/Downloads/levels2.txt','w') as f2:
-        for i in range(115):
-            line=f1.readline()
-            line='"'+line[0:-5]+'",'
-            f2.writelines(line+"\n")
-            print(line)
-
